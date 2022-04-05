651fc009f351106ea47ccfcdc42f4bab08af4a02
gjsl <103027542+gjsl@users.noreply.github.com>
2022-04-05 10:03:34 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,61 @@
-
+let terningen = 0
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        terningen = randint(1, 6)
+        basic.showNumber(terningen)
+    }
+    if (terningen == 6) {
+        basic.showLeds(`
+            . . . # .
+            . . # . .
+            . # # # .
+            # . . . #
+            . # # # .
+            `)
+    }
+    if (terningen == 5) {
+        basic.showLeds(`
+            # # # # #
+            # . . . .
+            # # # # .
+            . . . . #
+            # # # # .
+            `)
+    }
+    if (terningen == 4) {
+        basic.showLeds(`
+            . . # # .
+            . # . # .
+            # . . # .
+            # # # # #
+            . . . # .
+            `)
+    }
+    if (terningen == 3) {
+        basic.showLeds(`
+            # # # # .
+            . . . # .
+            . . # . .
+            # . . # .
+            . # # . .
+            `)
+    }
+    if (terningen == 2) {
+        basic.showLeds(`
+            # # # . .
+            . . . # .
+            . # # . .
+            # . . . .
+            # # # # .
+            `)
+    }
+    if (terningen == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+    }
+})
